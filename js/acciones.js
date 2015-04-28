228b235a95f38b7faf723afb883b4839a5d7006d
AL37F13 <lucero.rc.16@gmail.com>
2015-04-28 13:27:01 -0500

    dos
    subida dos

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,12 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+$('#btnvibrar').on('tap',function(){
+	navigator.vibrate(1000);
+});//tap a btnvibrar
+$('#btnbeep').on('tap',function(){
+	navigator.beep(1);
+});//tap a btnbeep
 }); 
 });
 
